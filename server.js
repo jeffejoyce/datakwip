55abae8bd7dd622be3fb0f7e4e22826770b99432
Jeff Joyce <jeff.joyce@t1cg.com>
2020-04-27 07:41:09 -0400

    server

--- server.js
+++ server.js
@@ -5,4 +5,4 @@ app.use(express.static(path.join(__dirname, 'build')));
 app.get('/', function(req, res) {
   res.sendFile(path.join(__dirname, 'build', 'index.html'));
 });
-app.listen(3000);
+app.listen(5000);
